fb4130c5a1eb35460ca656454dad0e7ca0aa2fe1
mateoruiz5171 <mateoruiz5171@gmail.com>
2022-11-18 00:04:14 -0600

    name update

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int t;
+int n;
+
+void sol()
+{
+}
+
+int main()
+{
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        sol();
+    }
+
+    return 0;
+}
